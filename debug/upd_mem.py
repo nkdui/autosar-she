3f27e9afd221a82bdf63af33523bfb08092b277e
Nguyen Khac Duy (MS/ETA-Hub-JP) <Duy.NguyenKhac2@vn.bosch.com>
2025-08-05 22:12:07 +0700

    update test script

--- debug/upd_mem.py
+++ debug/upd_mem.py
@@ -1,15 +1,55 @@
 from autosar_she import MemorySlot, ProtectionFlag, memory_update
 from autosar_she.algo import kdf
 
+
+print("Master key")
 m12345 = memory_update(
-    bytes.fromhex("000000000000000000000000000001"),
+    bytes.fromhex("000000000000000000000000000000"),
     MemorySlot.MASTER_ECU_KEY,
-    bytes.fromhex("000102030405060708090a0b0c0d0e0f"),
-    MemorySlot.KEY_1,
-    bytes.fromhex("0f0e0d0c0b0a09080706050403020100"),
+    bytes.fromhex("00000000000000000000000000000000"),
+    MemorySlot.MASTER_ECU_KEY,
+    bytes.fromhex("2B7E151628AED2A6ABF7158809CF4F3C"),
     1,
     ProtectionFlag.NONE,
 )
+print(b"".join(m12345[:3]).hex())
+print(b"".join(m12345[3:]).hex())
 
-for mi in m12345:
-    print(mi.hex())
+print("SW update key")
+m12345 = memory_update(
+    bytes.fromhex("000000000000000000000000000000"),
+    MemorySlot.MASTER_ECU_KEY,
+    bytes.fromhex("2B7E151628AED2A6ABF7158809CF4F3C"),
+    MemorySlot.KEY_1,
+    bytes.fromhex("06A9214036B8A15B512E03D534120006"),
+    1,
+    ProtectionFlag.NONE,
+)
+print(b"".join(m12345[:3]).hex())
+print(b"".join(m12345[3:]).hex())
+
+print("REE MAC key")
+m12345 = memory_update(
+    bytes.fromhex("000000000000000000000000000000"),
+    MemorySlot.MASTER_ECU_KEY,
+    bytes.fromhex("2B7E151628AED2A6ABF7158809CF4F3C"),
+    MemorySlot.KEY_2,
+    bytes.fromhex("06A9214036B8A15B512E03D534120006"),
+    1,
+    ProtectionFlag.NONE | ProtectionFlag.KEY_USAGE | ProtectionFlag.CMAC_USAGE,
+)
+print(b"".join(m12345[:3]).hex())
+print(b"".join(m12345[3:]).hex())
+
+print("HL MAC key")
+m12345 = memory_update(
+    bytes.fromhex("000000000000000000000000000000"),
+    MemorySlot.MASTER_ECU_KEY,
+    bytes.fromhex("2B7E151628AED2A6ABF7158809CF4F3C"),
+    MemorySlot.KEY_3,
+    bytes.fromhex("06A9214036B8A15B512E03D534120006"),
+    1,
+    ProtectionFlag.NONE | ProtectionFlag.KEY_USAGE,
+)
+print(b"".join(m12345[:3]).hex())
+print(b"".join(m12345[3:]).hex())
